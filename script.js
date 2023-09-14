fe32231343833e39da94b60e984c7e1291f740c6
karakayamert949 <karakayamert949@gmail.com>
2023-09-14 20:24:08 +0300

    Add getPlayer choice and game functions

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
-    return choices[Math.floor(Math.random() * 3)];
+    return choices[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection,computerSelection){
     if (playerSelection.toLowerCase() === computerSelection){
         return 'Draw! You both chose '+ computerSelection
-
     }
     else {
         switch (playerSelection.toLowerCase()){
@@ -17,6 +16,35 @@ function playRound(playerSelection,computerSelection){
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function getPlayerChoice(){
+    while (true) {
+        let playerChoice = prompt('Choose between rock, paper and scissors');
+        if (['rock', 'paper', 'scissors'].includes(playerChoice.toLowerCase())){
+            return playerChoice}
+        else console.log('Invalid choice! Please select from rock paper scissors');
+    }
+}
+
+function game(roundNum){
+    let playerScore=0;
+    for (let round=0; round<roundNum; round++){
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        let res= playRound(playerChoice,computerChoice);
+        console.log(res);
+        if (res[4]==='w'){
+            playerScore+=1;
+        }
+    }
+    if (playerScore > roundNum/2){
+        console.log('You won the game! You won', playerScore, 'games out of', roundNum);
+    }
+    else if (playerScore < roundNum/2){
+        console.log('You lost the game! You won', playerScore, 'games out of', roundNum);
+    }
+    else { 
+        console.log('Draw! You won', playerScore, 'games out of', roundNum);
+    }  
+}
+
+game(2)
