e7db629dccae288f13e48746d74bc790c8411b73
karakayamert949 <karakayamert949@gmail.com>
2023-09-28 18:20:18 +0300

    Add ui

--- script.js
+++ script.js
@@ -3,48 +3,66 @@ function getComputerChoice(){
     return choices[Math.floor(Math.random() * 3)]
 }
 
-function playRound(playerSelection,computerSelection){
-    if (playerSelection.toLowerCase() === computerSelection){
-        return 'Draw! You both chose '+ computerSelection
+function playRound(playerChoice,computerChoice){
+    console.log(playerChoice,computerChoice)
+    if (playerChoice.toLowerCase() === computerChoice){
+        return 'Draw! You both chose '+ computerChoice
     }
     else {
-        switch (playerSelection.toLowerCase()){
-            case 'rock': return computerSelection == 'paper' ? 'You lose! Paper beats rock' : 'You win! Rock beats scissors'
-            case 'paper': return computerSelection == 'rock' ? 'You win! Paper beats rock' : 'You lose! Scissors beat paper'
-            case 'scissors': return computerSelection == 'paper' ? 'You win! Paper beats scissors' : 'You lose! Rock beats scissors'
+        switch (playerChoice.toLowerCase()){
+            case 'rock': return computerChoice == 'paper' ? 'You lose! Paper beats rock' : 'You win! Rock beats scissors'
+            case 'paper': return computerChoice == 'rock' ? 'You win! Paper beats rock' : 'You lose! Scissors beat paper'
+            case 'scissors': return computerChoice == 'paper' ? 'You win! Paper beats scissors' : 'You lose! Rock beats scissors'
         }
     }
 }
-
-function getPlayerChoice(){
-    while (true) {
-        let playerChoice = prompt('Choose between rock, paper and scissors');
-        if (['rock', 'paper', 'scissors'].includes(playerChoice.toLowerCase())){
-            return playerChoice}
-        else console.log('Invalid choice! Please select from rock paper scissors');
+function checkWinner(playerScore,computerScore){
+    if (playerScore > 4){
+        let gameRes= document.createElement('p');
+        gameRes.textContent=`You won the game! You won ${playerScore} games`;
+        resultContainer.appendChild(gameRes);
     }
-}
-
-function game(roundNum){
-    let playerScore=0;
-    for (let round=0; round<roundNum; round++){
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let res= playRound(playerChoice,computerChoice);
-        console.log(res);
-        if (res[4]==='w'){
-            playerScore+=1;
-        }
+    else if (computerScore > 4){
+        let gameRes= document.createElement('p');
+        gameRes.textContent=`You lost the game! You won ${playerScore} games`;
+        resultContainer.appendChild(gameRes);
     }
-    if (playerScore > roundNum/2){
-        console.log('You won the game! You won', playerScore, 'games out of', roundNum);
-    }
-    else if (playerScore < roundNum/2){
-        console.log('You lost the game! You won', playerScore, 'games out of', roundNum);
-    }
-    else { 
-        console.log('Draw! You won', playerScore, 'games out of', roundNum);
+    else if (computerScore===5 && playerScore===5){ 
+        let gameRes= document.createElement('p');
+        gameRes.textContent=`Draw! You won ${playerScore} games`;
+        resultContainer.appendChild(gameRes);
     }  
 }
 
-game(2)
+function updtScore(res){
+    if (res.textContent[4]==='w'){
+        playerScore+=1;
+    }
+    else{
+        computerScore+=1;
+    }
+}
+
+let resultContainer = document.querySelector('.resultContainer');
+let playerScore=computerScore=0;
+
+let score= document.createElement('p');
+
+let selectBtns = document.querySelectorAll('.selectBtn');
+
+selectBtns.forEach((selectBtn) => {
+    selectBtn.addEventListener('click', 
+    () => {let playerChoice=selectBtn.textContent;
+            let computerChoice=getComputerChoice();
+            let res = document.createElement('p');
+            res.textContent= playRound(playerChoice,computerChoice);
+            resultContainer.appendChild(res);
+            updtScore(res);
+
+            score.textContent= `Player ${playerScore} vs ${computerScore} Computer`;
+
+            checkWinner(playerScore,computerScore);
+        })});
+
+
+resultContainer.appendChild(score);
